3554c3c899606dd6593cb8314f4bee4fa8c95ccb
Roshan <roshanramdas@gmail.com>
2023-03-30 12:11:31 -0400

    Added Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRergCTOVXM_z88_AL196jP1RNQGp0_Ks",
+  authDomain: "getcheckgo-madt.firebaseapp.com",
+  projectId: "getcheckgo-madt",
+  storageBucket: "getcheckgo-madt.appspot.com",
+  messagingSenderId: "1032774099477",
+  appId: "1:1032774099477:web:9c48f6d0323e931f04a41c",
+  measurementId: "G-RTRP9NBB0M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
